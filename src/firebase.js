c872cd6ee43f394adb4b0bb0bfc9cff978bd9a1f
EDGAR <edgar@Chinemes-MacBook-Pro.local>
2021-06-15 19:58:33 +0100

    no keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABu-pt-VKo8w-osnWyXwBZTZVfJ5JZPPI",
-  authDomain: "burnt-orange-77f2e.firebaseapp.com",
-  projectId: "burnt-orange-77f2e",
-  storageBucket: "burnt-orange-77f2e.appspot.com",
-  messagingSenderId: "745752994457",
-  appId: "1:745752994457:web:c44fcaf54e75be97ef4e08",
-  measurementId: "G-9Y76SNDRM8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
